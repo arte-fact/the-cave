362bb2609bbc01b1f1f7a5ae95dc473067dccee4
Benoit Zuinghedau <benoit.zuinghedau@soyhuce.fr>
2024-10-31 12:19:36 +0100

    update server ip

--- src/main.rs
+++ src/main.rs
@@ -98,7 +98,7 @@ fn handle_connection(stream: TcpStream) -> String {
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let listener = TcpListener::bind("127.0.0.1:7878")?;
+    let listener = TcpListener::bind("0.0.0.0:7878")?;
     for stream in listener.incoming() {
         match stream {
             Err(e) => eprintln!("Error: {}", e),
